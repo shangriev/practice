7d1fdaaf42c3b784da0e75841eaa206495a3b95d
shangriev <shangriev@gmail.com>
2023-04-11 18:15:23 +0500

    data structuring

--- React/js/index.js
+++ React/js/index.js
@@ -1,7 +1,13 @@
-ReactDOM.render(React.createElement('input', {
+// ReactDOM.render(React.createElement('input', {
 
-    placeholder: "Введите текст", 
-    onClick: () => console.log("Clicked"),
-    onMouseEnter: () => console.log("Mouse Over"),
+//     placeholder: "Введите текст", 
+//     onClick: () => console.log("Clicked"),
+//     onMouseEnter: () => console.log("Mouse Over"),
 
-}), document.getElementById("app"));
+// }), document.getElementById("app"));
+
+
+const newElement = <input placeholder="Введите текст" />;
+const app = document.getElementById("app");
+
+ReactDOM.render(newElement, app);
