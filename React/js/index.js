a09731d04fecef20a5236d9a76a27bd9a32290bb
shangriev <shangriev@gmail.com>
2023-04-11 18:39:21 +0500

    local connection of the React and the ReactDom

--- React/js/index.js
+++ React/js/index.js
@@ -7,7 +7,22 @@
 // }), document.getElementById("app"));
 
 
-const newElement = <input placeholder="Введите текст" />;
+const inputClick = () => console.log("Клик");
+const inputMoveMouse = () => console.log("Наведение мышки");
+const enterText = "Введите текст"
+
+const newElement = (<div>
+
+<h1>Заголовок</h1>
+
+<input placeholder={enterText} 
+onClick={inputClick} 
+onMouseEnter={inputMoveMouse} />
+
+<p>Какое-то описание сайта</p>
+
+</div> )
+
 const app = document.getElementById("app");
 
 ReactDOM.render(newElement, app);
